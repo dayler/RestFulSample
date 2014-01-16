61e5dd951cdf59ee1f9ee585a4eb5ff68ece8af2
Ariel Salazar <ariel.salazar@nuevatel.com>
2014-01-15 21:33:27 -0400

    finish with todos resource

--- src/com/dayler/sample/web/restful/TodosResource.java
+++ src/com/dayler/sample/web/restful/TodosResource.java
@@ -3,11 +3,17 @@
  */
 package com.dayler.sample.web.restful;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -64,4 +70,30 @@ public class TodosResource {
         return String.valueOf(count);
     }
 
+    @POST
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void newTodo(
+            @FormParam("id") String id,
+            @FormParam("summary") String summary,
+            @FormParam("description") String description,
+            @Context HttpServletResponse servletResponse) throws IOException {
+        Todo todo = new Todo(id, summary);
+
+        if (description != null) {
+            todo.setDescription(description);
+        }
+
+        TodoDao.instance.getModel().put(id, todo);
+        servletResponse.sendRedirect("../create_todo.html");
+    }
+
+    // Defines that the next path parameter after todos is
+    // treated as a parameter and passed to the TodoResources
+    // Allows to type http://localhost:8080/de.vogella.jersey.todo/rest/todos/1
+    // 1 will be treaded as parameter todo and passed to TodoResource
+    @Path("{todo}")
+    public TodoResource getTodo(@PathParam("todo") String id) {
+        return new TodoResource(uriInfo, request, id);
+    }
 }
